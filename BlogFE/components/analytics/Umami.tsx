a0fdbd45923fd35f8bb7674ea2f8eb46c8cbb164
ThangChiba <thangchiba@gmail.com>
2024-02-20 00:36:42 +0900

    Modify umami read url

--- BlogFE/components/analytics/Umami.tsx
+++ BlogFE/components/analytics/Umami.tsx
@@ -2,11 +2,6 @@ import Script from 'next/script'
 import { siteMetadata } from '~/data/siteMetadata'
 
 export function UmamiScript() {
-  return (
-    <Script
-      async
-      data-website-id={siteMetadata.analytics.umamiWebsiteId}
-      src="http://api.thangchiba.com:51211/umami.js"
-    />
-  )
+  const umamiURL = process.env.NEXT_PUBLIC_UMAMI_URL || ''
+  return <Script async data-website-id={siteMetadata.analytics.umamiWebsiteId} src={umamiURL} />
 }
