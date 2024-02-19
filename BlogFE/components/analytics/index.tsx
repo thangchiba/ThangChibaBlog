ea0ecfee2ad2d48ef35d18dfdf679e17b70e0117
ThangChiba <thangchiba@gmail.com>
2024-02-19 23:56:09 +0900

    Analystic on any env

--- BlogFE/components/analytics/index.tsx
+++ BlogFE/components/analytics/index.tsx
@@ -6,16 +6,13 @@ import { UmamiScript } from './Umami'
 let isProduction = process.env.NODE_ENV === 'production'
 
 export function Analytics() {
-  if (isProduction) {
-    let { analytics } = siteMetadata
-    let { simpleAnalytics, umamiWebsiteId, googleAnalyticsId } = analytics
-    return (
-      <>
-        {simpleAnalytics && <SimpleAnalyticsScript />}
-        {umamiWebsiteId && <UmamiScript />}
-        {googleAnalyticsId && <GAScript />}
-      </>
-    )
-  }
-  return null
+  let { analytics } = siteMetadata
+  let { simpleAnalytics, umamiWebsiteId, googleAnalyticsId } = analytics
+  return (
+    <>
+      {simpleAnalytics && <SimpleAnalyticsScript />}
+      {umamiWebsiteId && <UmamiScript />}
+      {googleAnalyticsId && <GAScript />}
+    </>
+  )
 }
