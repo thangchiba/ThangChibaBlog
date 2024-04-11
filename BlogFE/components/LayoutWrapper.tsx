1986215962c696f534e6c8fff08ddda888197473
ThangChiba <thangchiba@gmail.com>
2024-04-11 23:33:54 +0900

    Fix layout

--- BlogFE/components/LayoutWrapper.tsx
+++ BlogFE/components/LayoutWrapper.tsx
@@ -11,15 +11,15 @@ export function LayoutWrapper({ children }) {
 
   const router = useRouter()
   const path = router.pathname || ''
-  const maxXLWidthClass =
-    path === '/utils' || path === '/course'
-      ? 'xl:max-w-7xl' // Adjust the maximum width as needed
-      : 'xl:max-w-5xl'
+  // const maxXLWidthClass =
+  //   path === '/utils' || path === '/course'
+  //     ? 'md:max-w-6xl xl:max-w-7xl' // Adjust the maximum width as needed
+  //     : 'xl:max-w-5xl'
   return (
     <>
       <MobileNav navShow={navShow} onToggleNav={onToggleNav} />
       <Header navShow={navShow} onToggleNav={onToggleNav} />
-      <div className={`mx-auto max-w-3xl px-3 sm:px-6 ${maxXLWidthClass} xl:px-0`}>
+      <div className={`mx-auto max-w-3xl md:max-w-4xl xl:max-w-5xl  px-3 sm:px-6 xl:px-0`}>
         <div className="flex flex-col justify-between">
           <main style={{ minHeight: MAIN_CONTENT_MIN_HEIGHT }}>{children}</main>
           <Footer />
