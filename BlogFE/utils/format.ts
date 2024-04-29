489bc245a5c31a9b9f268e46c5ed6f01b4a38630
ThangChiba <thangchiba@gmail.com>
2024-04-30 00:41:43 +0900

    Fix bug format image sharing

--- BlogFE/utils/format.ts
+++ BlogFE/utils/format.ts
@@ -1,3 +1,7 @@
 export function createUrl(base, ...paths) {
-  return [base, ...paths].join('/').replace(/\/\/+/g, '/')
+  // First join all parts of the URL.
+  const url = [base, ...paths].join('/')
+
+  // Replace all instances of multiple slashes with a single slash, except after the protocol part.
+  return url.replace(/([^:]\/)\/+/g, '$1')
 }
